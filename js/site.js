bc1bead8ca5b6c646dc9d0989f90ed085a39f083
Charles Saylee <csaylee8@gmail.com>
2023-04-14 15:14:18 -0400

    added function to reverse user input

--- js/site.js
+++ js/site.js
@@ -1,12 +1,62 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
-    // alert(msg);
+// controller
+function getValue(){
 
-    Swal.fire(
-        {
-            backdrop:false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+    let userInput = document.getElementById('message').value;
+
+    let reverseInput = reverseString(userInput);
+
+    displayString(reverseInput);
 }
+
+function reverseString(message){
+  //  take a string, return it in reverse
+
+  let reversedMessage = '';
+
+  for(let index = message.length - 1; index >= 0; index--){
+    reversedMessage += message[index];
+
+  }
+  
+
+  return reversedMessage;
+}
+
+
+function displayString(reversedMessage) {
+
+  document.getElementById('msg').textContent = reversedMessage;
+  document.getElementById('alert').classList.remove('d-none');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function displayMessage() {
+  // let msg = document.getElementById("message").value;
+  // alert(msg);
+
+  // Swal.fire({
+    // backdrop: false,
+    // title: "Reverse",
+    // text: msg,
+  // });
+// }
